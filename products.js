3ac8f71b9b92bcff7d56a993fcba96170e2ca11b
jaysoni2810 <jaysoni1028@gmail.com>
2021-02-21 10:45:33 +0400

    pic

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ let products = [
         title: "Pet rat",
         description:'<em>Homie</em> rat live in my basement'+' rent free',
         price:'3DHS',
-        img:'pic1.JPG',
+        img:'pic1.jpg',
         stock: 10,
         availableInventory: 10,
         rating:4
@@ -14,7 +14,7 @@ let products = [
         title: "Pet rat's bro",
         description:"<em>Homie</em> rat's twin bro live in my basement"+" rent free",
         price:'3DHS',
-        img:'pic1.JPG',
+        img:'pic1.jpg',
         stock: 10,
         availableInventory: 10,
         rating:4
