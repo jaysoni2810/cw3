037dd72029a00e7d6512efc8a0c8fcffb09e697a
jaysoni2810 <jaysoni1028@gmail.com>
2021-02-21 09:18:46 +0400

    icon added

---
+++ products.js
@@ -0,0 +1,22 @@
+let products = [
+    {
+        id: 1001,
+        title: "Pet rat",
+        description:'<em>Homie</em> rat live in my basement'+' rent free',
+        price:'3DHS',
+        img:'rat.jpg',
+        stock: 10,
+        availableInventory: 10,
+        rating:4
+    },
+    {
+        id: 1002,
+        title: "Pet rat's bro",
+        description:"<em>Homie</em> rat's twin bro live in my basement"+" rent free",
+        price:'3DHS',
+        img:'rat.jpg',
+        stock: 10,
+        availableInventory: 10,
+        rating:4
+    }
+]
