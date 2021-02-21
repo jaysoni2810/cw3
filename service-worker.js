8d95c96678f68e7361d5895a82cbd5a556afabda
jaysoni2810 <jaysoni1028@gmail.com>
2021-02-21 11:19:45 +0400

    fixed

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,6 @@ var cacheFiles = [
     'product.js',
     'petstore1.webmanifest',
     'img/pic1.jpg',
-    'img/pic1.jpg',
     'img/icon.jpg',
 ];
 
@@ -28,17 +27,3 @@ self.addEventListener('install', (e) => {
 //     );
 // }​​);
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (r) {
-            // Download the file if it is not in the cache, 
-            return r || fetch(e.request).then(function (response) {
-                // add the new file to cache
-                return caches.open(cacheName).then(function (cache) {
-                    cache.put(e.request, response.clone());
-                    return response;
-                }​​);
-            }​​);
-        }​​)
-    );
-}​​);
